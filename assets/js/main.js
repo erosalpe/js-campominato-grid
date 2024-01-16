8ec8d73fcf5eb442c909c0dc239ad92ceaf6e68d
erosalpe <eros.alpegiani@gmail.com>
2024-01-16 15:50:18 +0100

    feat(griglia) aggiunto cambio colore al click e difficoltà diverse

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,75 @@
-const grigliaHtml = document.getElementById("griglia")
+const grigliaHtml = document.getElementById("griglia");
+const buttonGioca = document.getElementById("gioca");
+const difficultyChoose = document.getElementById("difficulty");
+var r = document.querySelector(':root');
+
+
+buttonGioca.addEventListener( "click", function(){
+    grigliaHtml.innerHTML = "";
+
+    if (difficultyChoose.value === "difficoltà1"){
+
+        for (let i = 1; i <= 100; i++){
+            let box = document.createElement("div");
+            box.classList.add("box");
+
+            box.addEventListener( "click", function(){
+                this.classList.toggle("active");
+            })
+
+            box.innerText = i;
+        
+            grigliaHtml.append(box);
+        }
+
+    } else if (difficultyChoose.value === "difficoltà2"){
+        r.style.setProperty('--columns', '9');
+
+        for (let i = 1; i <= 81; i++){
+            let box = document.createElement("div");
+            box.classList.add("box");
+            box.addEventListener( "click", function(){
+                this.classList.toggle("active");
+            })
+
+            box.innerText = i;
+        
+            grigliaHtml.append(box);
+        }
+
+    } else if (difficultyChoose.value === "difficoltà3"){
+        r.style.setProperty('--columns', '7');
+
+        for (let i = 1; i <= 49; i++){
+            let box = document.createElement("div");
+            box.classList.add("box");
+            box.addEventListener( "click", function(){
+                this.classList.toggle("active");
+            })
+
+            box.innerText = i;
+        
+            grigliaHtml.append(box);
+        }
+    }
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+
+
+
